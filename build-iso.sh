f8c5908f8b19121d7dbfe8f46a0c145edde5ecf8
Peter Liveyns <peter.liveyns@gmail.com>
2024-06-05 20:26:21 +0200

    20240604 Add local dir to .gitignore

---
+++ build-iso.sh
@@ -0,0 +1,7 @@
+mkdir ./iso-output
+sudo podman run --rm --privileged --volume ./iso-output:/build-container-installer/build --security-opt label=disable --pull=newer \
+ghcr.io/jasonn3/build-container-installer:latest \
+IMAGE_REPO=ghcr.io/pliveyns \
+IMAGE_NAME=mybluebuild \
+IMAGE_TAG=latest \
+VARIANT=Silverblue # should match the variant your image is based on
