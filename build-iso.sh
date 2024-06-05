f2a096fc9c0dd8b9a1376330e0e0fc3e23fd6275
Peter Liveyns <peter.liveyns@gmail.com>
2024-06-05 22:09:33 +0200

    20240605 Modify iso build script

--- build-iso.sh
+++ build-iso.sh
@@ -1,3 +1,4 @@
+rm -Rf ./iso-output
 mkdir ./iso-output
 sudo podman run --rm --privileged --volume ./iso-output:/build-container-installer/build --security-opt label=disable --pull=newer \
 ghcr.io/jasonn3/build-container-installer:latest \
