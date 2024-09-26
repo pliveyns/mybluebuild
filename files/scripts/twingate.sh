436b946735e4d9d7780fe935c7499618159a3c8a
Peter Liveyns <peter.liveyns@gmail.com>
2024-09-26 21:42:55 +0200

    20240926 Added script to install twingate vpn

---
+++ files/scripts/twingate.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+echo 'Download and install twingate install script'
+curl -Lo /tmp/twingate.sh https://binaries.twingate.com/client/linux/install.shell
+bash /tmp/twingate.shell
+
