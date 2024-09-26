435091c572d23c3a85baba9a612943c5c0a8a56a
Peter Liveyns <peter.liveyns@gmail.com>
2024-09-26 22:14:46 +0200

    20240926 Debug twingate install script

--- files/scripts/twingate.sh
+++ files/scripts/twingate.sh
@@ -8,6 +8,6 @@ set -oue pipefail
 echo 'Add twingate repo and install package'
 rpm-ostree install -y 'dnf-command(config-manager)'
 ostree remote add twingate https://packages.twingate.com/rpm/
-rpm-ostree cleanup -m
+#rpm-ostree cleanup -m
 rpm-ostree install twingate
 
